185daf6e5c62bef252f8c27cc9b79be7fc9e20ed
Sagit <ourfor@foxmail.com>
2018-09-18 22:36:26 +0800

    add the map application

---
+++ src/Map.java
@@ -0,0 +1,10 @@
+public class Map {
+    public static void main(String args[]) {
+        System.out.println("-----------------------------------------------");
+        System.out.println("=                                             =");
+        System.out.println("=               Start Game                    =");
+        System.out.println("=                                             =");
+        System.out.println("-----------------------------------------------");
+    }
+}
+
